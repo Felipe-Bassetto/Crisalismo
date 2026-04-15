03266497160f9578a135048ad76e9447c22ee9e2
Felipe Bassetto <felipe.bassetto2000@gmail.com>
2026-04-15 19:24:37 -0300

    Feat: Configurações

---
+++ Assets/Jogo/Scripts/Menu/TelaInicialManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelaInicialManager : MonoBehaviour
+{
+    [Header("Objetos")]
+    public GameObject opcoes;
+    public GameObject btnOpcoes;
+    public GameObject btnJogar;
+    public GameObject btnSair;
+    public GameObject nameGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenOpcoes()
+    {
+        opcoes.SetActive(true);
+        btnOpcoes.SetActive(false);
+        btnJogar.SetActive(false);
+        btnSair.SetActive(false);
+    }
+
+    public void FecharOpcoes()
+    {
+        opcoes.SetActive(false);
+        btnOpcoes.SetActive(true);
+        btnJogar.SetActive(true);
+        btnSair.SetActive(true);
+    }
+
+    public void Jogar()
+    {
+        nameGame.SetActive(false);
+        btnOpcoes.SetActive(false);
+        btnJogar.SetActive(false);
+        btnSair.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
