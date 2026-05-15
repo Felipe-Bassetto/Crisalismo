38875e78112a6b12f5333fd9945d25c71766b7f5
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2026-05-15 16:43:49 -0300

    Feat: colocando decorações

---
+++ Assets/Jogo/Scripts/Decoração/Decoration.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decoration : MonoBehaviour
+{
+    [Header("Variáveis")]
+    private LayerMask groundLayer;
+    public bool isCostructMode = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundLayer = LayerMask.GetMask("GroundConstruction");
+    }
+
+    // Update is called once per frame
+        
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer) && isCostructMode)
+        {
+            transform.position = hit.point;
+
+            if (Input.GetMouseButtonDown(0)) isCostructMode = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.forward = Camera.main.transform.forward;
+    }
+}
