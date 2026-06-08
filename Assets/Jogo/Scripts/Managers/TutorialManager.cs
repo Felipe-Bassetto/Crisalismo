7dfe19687c2c7e6db4d7f7daccba079c7c8f9f1f
Lauro Tomiatti <tomiatti1@hotmail.com>
2026-06-08 00:30:50 -0300

    inicio da criacao de tutorial

---
+++ Assets/Jogo/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using TMPro;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textoDoTutorial;
+
+    private int passoAtual = 0;
+
+    private string[] passos =
+    {
+        "",
+        "Utilize A e D para mover a câmera.",
+        "Essa é sua quantidade de Sparkle.",
+        "Esse é o botão de construção. Vamos tentar!",
+        "Escolha Palco de Teatro e a posicione no tabuleiro.",
+        "Esse contador mostra quantas crianças estão brincando no forte.",
+        "Cada criança também gera Sparkles com o tempo.",
+        "Construa a primeira decoração.",
+        "Veja as necessidades das outras crianças.",
+        "Inicie uma interação com a criança.",
+        "Esse é seu vínculo com a criança. Há diversas maneiras de se aumentar o vínculo com uma criança.",
+        "Inicie um minigame com a criança para aumentar seu vínculo.",
+        "Ao anoitecer, as crianças brincam automaticamente.",
+        "O Forte está vivo, as crianças brincando, os objetos são transformados pela imaginação! O tutorial está completo."
+    };
+
+    private string[] falas =
+    {
+        "Oi, está aí? Está conseguindo enxergar?",
+        "Todo Forte começa pequeno, mas quando várias crianças brincam juntas, ele pode se tornar o mundo inteiro para elas.",
+        "Quando a gente imagina junto, as coisas ganham vida!",
+        "Olha, o pessoal está chegando! Quanto maior o forte, mais crianças querem brincar aqui!",
+        "Poxa, o Forte parece vazio.",
+        "Você vai continuar vindo brincar aqui amanhã?",
+        "Venha, vamos jogar um jogo!",
+        "Agora esse lugar é especial!"
+    };
+
+    void Start()
+    {
+        MostrarPasso();
+    }
+
+    public void ProximoPasso()
+    {
+        passoAtual++;
+
+        if (passoAtual >= passos.Length)
+        {
+            textoDoTutorial.gameObject.SetActive(false);
+            return;
+        }
+
+        MostrarPasso();
+    }
+
+    private void MostrarPasso()
+    {
+        textoDoTutorial.text = passos[passoAtual];
+    }
+
+    void Update()
+    {
+        if (passoAtual == 0 && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            // setar a câmera com X 38, Y -190, Z -257
+            // falas[0]
+            //
+            ProximoPasso();
+        }
+
+        if (passoAtual == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            // falas[1]
+            // colocar png do tutorial para mostrar o sparkle
+            ProximoPasso();
+        }
+
+        if (passoAtual == 2
+            // && detectar botao de construcao precionado detectar objeto de construcao criado
+            )
+        {
+            // falas[2]
+            // ativar botão de construção
+            // colocar png no botão de construção 
+            ProximoPasso();
+        }
+
+        if (passoAtual == 3
+            // && detectar objeto de construcao criado
+            )
+        {
+            // colocar png botao do palco 
+            ProximoPasso();
+        }
+    }
+}
